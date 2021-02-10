5a0f7591f0c53cb86b52ea4cb76bcaebc14453fe
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-02-10 15:58:04 +0200

    Update

--- app/src/main/java/com/verygoodsecurity/auto/auto/testmavencentral/MainActivity.kt
+++ app/src/main/java/com/verygoodsecurity/auto/auto/testmavencentral/MainActivity.kt
@@ -2,10 +2,15 @@ package com.verygoodsecurity.auto.auto.testmavencentral
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.verygoodsecurity.auto.auto.mvncntrltst.Echo
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvHelloWorld?.setOnClickListener {
+            Echo.echo()
+        }
     }
 }
