121cff2336df84fcdf56cbebf46a985069320c26
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-02-26 11:50:10 +0200

    Implement publish

--- app/src/main/java/com/mvntst/auto/auto/testmavencentral/MainActivity.kt
+++ app/src/main/java/com/mvntst/auto/auto/testmavencentral/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.mvntst.auto.auto.testmavencentral
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.mvntst.auto.auto.mvncntrltst.Echo
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         tvHelloWorld?.setOnClickListener {
-            Echo.echo()
+            tvHelloWorld?.echo()
         }
     }
 }
---
+++ mvncntrltst/src/main/java/com/mvntst/auto/auto/mvncntrltst/EchoView.kt
@@ -0,0 +1,16 @@
+package com.mvntst.auto.auto.mvncntrltst
+
+import android.content.Context
+import android.util.AttributeSet
+import com.google.android.material.textview.MaterialTextView
+
+class EchoView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : MaterialTextView(context, attrs, defStyleAttr) {
+
+    fun echo() {
+        Echo.echo()
+    }
+}
